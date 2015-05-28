9a05631bfaf72413f3d68e56379eb7214b41e3fa
Abdi Ahmed <abdi.cg.ahmed@gmail.com>
2015-05-28 12:49:36 +0100

    added define path for server to call ac.define

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var index = fs.readFileSync(__dirname + '/index.html');
 http.createServer(function handler(request, response) {
   var url = request.url;
   console.log("request.url:", url);
+  
   if (url.length === 1) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.end(index.toString());
@@ -30,16 +31,27 @@ http.createServer(function handler(request, response) {
     // response.end('word: ', word);
   }
 
-  if(url.indexOf('/test/')){
-  	ac.define('shirt', function(err, definition){
+ //  if(url.indexOf('/test/')){
+ //  	ac.define('shirt', function(err, definition){
   		 
-  		response.end(definition[0].text); 
-  		console.log('definition: '+definition[0].text);  
-  	}); 
+ //  		response.end(definition[0].text); 
+ //  		//console.log('definition: '+definition[0].text);  
+ //  	}); 
   		
   		
-	// console.log('your requested this url: '+ url); 
-	// console.log('------------------------------')
+	// // console.log('your requested this url: '+ url); 
+	// // console.log('------------------------------')
+ //  } 
+
+  if (url.indexOf('/define/') > -1) {
+    // locahost:3000/find/word
+    word = url.split('/')[2];
+    // console.log(word);
+    ac.define(word, function (err, definition){
+      //console.log(definition);
+      response.end(definition[0].text);
+    });
+    // response.end('word: ', word);
   }
 
   else {
