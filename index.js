04a252db24ba11129f94dc66415c34c3e14cc92d
Jack.Rans <jwrans@gmail.com>
2015-05-28 12:22:33 +0100

    changed package.json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var http = require('http'); 
+var http = require('http');
 var ac = {};
 
 ac.import = function(callback){ // saving import fct as a property of ac{}
@@ -11,7 +11,7 @@ ac.import = function(callback){ // saving import fct as a property of ac{}
     ac.words = data.split('\n'); // stores array of words in ac{}
     return callback(err, ac.words); // call callback function on ac.words
   });
-}
+};
 
 ac.stats = function(word, callback){
   if (!ac.searches){
@@ -22,7 +22,7 @@ ac.stats = function(word, callback){
   }
   ac.searches[word].push(new Date().getTime());
   callback(null, ac.searches);
-}
+};
 
 ac.findWord = function (word, callback){
   //var found = ac.words[ac.words.indexOf(word)];
@@ -33,18 +33,18 @@ ac.findWord = function (word, callback){
     }
   }
   return callback(null,found);
-}
+};
 
 // record timestamp at which a search was made
 
 //define method on ac module
 ac.define = function (word, callback){
-	var wordDef
-	var body = ''; 
+	var wordDef;
+	var body = '';
 	var url = 'http://api.wordnik.com/v4/word.json/'+ word +'/definitions?limit=1&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
 
 	var request = http.get(url, function(response) {
-		
+
 		//Read the data
         response.on('data', function (chunk) {
             body += chunk;
@@ -53,18 +53,18 @@ ac.define = function (word, callback){
 
         response.on('end', function () {
             if(response.statusCode === 200) {
-               
+
                 wordDef = JSON.parse(body);
                 //console.log(body);
                 return callback(null, wordDef);
             }
         });
 
-		
+
 	});
 
-	 
-	
-}
+
+
+};
 
 module.exports = ac;// exporting ac object to use elsewhere
