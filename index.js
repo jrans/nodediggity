3d0863850d855ba1e8521a4a7adc69ea5aa805a6
Jack.Rans <jwrans@gmail.com>
2015-05-29 14:24:25 +0100

    created an ajx call for our api on backside

--- index.js
+++ index.js
@@ -28,10 +28,14 @@ ac.stats = function(word, callee, callback){
 ac.findWord = function (word, callback){
   //var found = ac.words[ac.words.indexOf(word)];
   var found = [];
+  // var otherfound=[];
   for (var i = 0; i < ac.words.length; i++){
     if (ac.words[i].search(word) === 0){
       found.push(ac.words[i]);
     }
+    // else if (ac.words[i].search(word) > 0){
+    //   otherfound.push(ac.words[i]);
+    // }
   }
   return callback(null,found);
 
